ed6ec8ca28fd552928b4cc9e1aa4ed99ed2adf47
Palika-Sathish <113234133+Palika-Sathish@users.noreply.github.com>
2022-10-30 22:36:23 -0400

    feat: created signup page

--- src/components/Sign-up.tsx
+++ src/components/Sign-up.tsx
@@ -1,20 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, SafeAreaView, TouchableOpacity, Text, View, Alert } from 'react-native';
+import Header from './Header';
 
-export default function Signup() {
+export default function Signup({ navigation }) {
+
+  const [emailAddress, setEmailAddress] = useState("");
+  const [confirmEmailAddress, setConfirmEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Sign Up Page</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header />
+      <View style={styles.bodyContainer}>
+        <Text style={styles.signUpTitle}>Sign Up</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setEmailAddress}
+          value={emailAddress}
+          placeholder="Email Address"
+          textContentType="emailAddress"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setConfirmEmailAddress}
+          value={confirmEmailAddress}
+          placeholder="Confirm Email Address"
+          textContentType="username"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setPassword}
+          value={password}
+          placeholder="Password"
+          textContentType="password"
+          secureTextEntry={true}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setConfirmPassword}
+          value={confirmPassword}
+          placeholder="Confirm Password"
+          textContentType="password"
+          secureTextEntry={true}
+        />
+        <View style={styles.buttonContainer} >
+          <TouchableOpacity style={styles.button} onPress={() => {
+            if (emailAddress !== confirmEmailAddress) {
+              Alert.alert("Mismatch", "please check wdfefyour email")
+            } else {
+              Alert.alert("Ohoo!!", "Your profile has been created...")
+              navigation.navigate("Sign In")
+            }
+          }}
+            disabled={emailAddress && confirmEmailAddress && password ? false : true}>
+            <Text style={styles.signUpText}>Sign up</Text>
+          </TouchableOpacity>
+        </View >
+      </View>
+    </SafeAreaView >
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  bodyContainer: {
+    backgroundColor: '#fff',
+    justifyContent: "center",
+    height: "100%"
+  },
+  signUpTitle: {
+    fontSize: 30,
+    textAlign: "center",
+    fontWeight: "600",
+    marginBottom: 30
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    borderLeftColor: "#FA6E3B",
+    borderLeftWidth: 5,
+  },
+  buttonContainer: {
+    padding: 10,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+
+  },
+  button: {
+    backgroundColor: "#FA6E3B",
+    padding: 10,
+    borderRadius: 10,
+    minWidth: 100
+  },
+  signUpText: {
+    color: "#fff",
+    textAlign: "center"
+  }
 });
+
